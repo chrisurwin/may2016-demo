050a182b565af33f99f49644fa9b5f7917324463
Chris Urwin <chris.urwin@hscic.gov.uk>
2016-05-23 14:19:28 +0100

    Pears vs Plums

--- voting-app/app.py
+++ voting-app/app.py
@@ -8,8 +8,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Oranges")
-option_b = os.getenv('OPTION_B', "Apples")
+option_a = os.getenv('OPTION_A', "Pears")
+option_b = os.getenv('OPTION_B', "Plums")
 hostname = socket.gethostname()
 
 redis = connect_to_redis("redis")
