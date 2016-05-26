fa6dae639f13329520562b18e869e825d606202b
Chris Urwin <chris.urwin@hscic.gov.uk>
2016-05-26 11:24:33 +0100

    Blue vs Green

--- voting-app/app.py
+++ voting-app/app.py
@@ -8,8 +8,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Red")
-option_b = os.getenv('OPTION_B', "Orange")
+option_a = os.getenv('OPTION_A', "Blue")
+option_b = os.getenv('OPTION_B', "Green")
 hostname = socket.gethostname()
 
 redis = connect_to_redis("redis")
